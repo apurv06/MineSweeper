261b501a95c0363e3ad014a5bd8e0044435fda7a
Apurv <apurv.chaudhary06@gmail.com>
2018-06-22 18:39:31 +0530

    Test commit

--- app/src/main/java/com/example/apurv/minesweeper/Home.java
+++ app/src/main/java/com/example/apurv/minesweeper/Home.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+//Comment Test for remote access
 public class Home extends AppCompatActivity implements View.OnClickListener{
 
 
